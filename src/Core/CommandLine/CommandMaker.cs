53e322bfcb1248ea0a5921ade08ff1d8d75eda2a
codesigner <codesigner.ds@gmail.com>
2025-09-13 09:01:15 +0800

    add

--- src/Core/CommandLine/CommandMaker.cs
+++ src/Core/CommandLine/CommandMaker.cs
@@ -13,11 +13,53 @@ internal static class ZgoCommandMaker
     {
         Type commandType = command.GetType();
         Type thisType = commandType;
-        while (typeof(IZgoCommand).IsAssignableFrom(thisType))
+        BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+        foreach (FieldInfo fieldInfo in thisType.GetFields(bindingFlags))
         {
-            MakeCommandHelper(thisType, command);
+            ZgoCommand.ArgumentAttribute arugmentAttr = fieldInfo.GetCustomAttribute<ZgoCommand.ArgumentAttribute>();
+            if (arugmentAttr == null)
+            {
+                continue;
+            }
+            Type fieldType = fieldInfo.FieldType;
+
+            object defaultValue = fieldInfo.GetValue(command);
+            if (arugmentAttr is ZgoCommand.OptionAttribute optionAttr)
+            {
+                Type optionType = typeof(Option<>).MakeGenericType(fieldType);
+                Option option = Activator.CreateInstance(optionType, optionAttr.Name, optionAttr.Aliases) as Option;
+                SetOption(option, optionAttr);
+                command.Options.Add(option);
+                
+            }
+            else
+            {
+                Type argumentType = typeof(Argument<>).MakeGenericType(fieldType);
+                Argument argument = Activator.CreateInstance(argumentType, argumentType.Name) as Argument;
+                SetArgument(argument, arugmentAttr);
+                command.Arguments.Add(argument);
+            }
         }
     }
+
+    private static void SetOption(Option option, ZgoCommand.OptionAttribute optionAttr)
+    {
+        option.Description = optionAttr.Description;
+        option.HelpName = optionAttr.HelpName;
+        option.Arity = optionAttr.Arity;
+        option.Recursive = optionAttr.Recursive;
+        option.AllowMultipleArgumentsPerToken = optionAttr.AllowMultipleArgumentsPerToken;
+        option.Hidden = optionAttr.Hidden;
+        option.Required = optionAttr.Required;
+    }
+
+    private static void SetArgument(Argument argument, ZgoCommand.ArgumentAttribute argumentAttr)
+    {
+        argument.Arity = argumentAttr.Arity;
+        argument.Description = argumentAttr.Description;
+        argument.HelpName = argumentAttr.HelpName;
+    }
+
 
     private static void MakeCommandHelper(Type commandType, IZgoCommand zgoCommand)
     {
--- src/Core/CommandLine/ZgoCommand.cs
+++ src/Core/CommandLine/ZgoCommand.cs
@@ -1,29 +1,28 @@
 ﻿namespace Zgo.Core;
 
 using System.CommandLine;
-using System.Reflection.Metadata;
 
 public class ZgoCommand : Command, IZgoCommand
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    protected class OptionAttribute : ArgumentAttribute
+    public class OptionAttribute : ArgumentAttribute
     {
 
         public bool Required { get; set; }
         public bool Recursive { get; set; }
+
+        public string[] Aliases { get; set; }
+        public bool Hidden { get; set; }
         public bool AllowMultipleArgumentsPerToken { get; set; }
     }
 
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-    protected class ArgumentAttribute : Attribute
+    public class ArgumentAttribute : Attribute
     {
         public ArgumentArity Arity { get; set; }
-        public string[] Alias { get; set; }
         public string[] AllowValues { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
-        public bool Hidden { get; set; }
 
         public string HelpName { get; set; }
 
