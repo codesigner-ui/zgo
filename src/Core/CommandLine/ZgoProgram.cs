c1ae564c8981a87a0d28720bbd7ec0bce6bd9945
codesigner <codesigner.ds@gmail.com>
2025-09-15 07:58:23 +0800

    Update ZgoProgram.cs

--- src/Core/CommandLine/ZgoProgram.cs
+++ src/Core/CommandLine/ZgoProgram.cs
@@ -5,8 +5,11 @@ namespace Zgo.Core;
 
 public class ZgoProgram : RootCommand, IZgoCommand
 {
+    public ParserConfiguration ParserConfiguration { get; set; } = new ParserConfiguration();
+    public InvocationConfiguration InvocationConfiguration { get; set; } = new InvocationConfiguration();
     public ZgoProgram(string description = "") : base(description)
     {
 
     }
+    
 }
