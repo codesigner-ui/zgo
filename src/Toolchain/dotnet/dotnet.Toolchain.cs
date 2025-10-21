e96b938427b9db7ac3df8140210c640d0c4afd5d
codesigner <codesigner.ds@gmail.com>
2025-10-22 01:02:36 +0800

    add

--- src/Toolchain/dotnet/dotnet.Toolchain.cs
+++ src/Toolchain/dotnet/dotnet.Toolchain.cs
@@ -5,6 +5,7 @@ public class DotnetCommand : ZgoCommand
 {
     public DotnetCommand() : base("dotnet", "dotnet toolchain")
     {
+        this.Add(new DotnetInstallCommand());
         this.Add(new PackageDownloadCommand());
         this.Add(new DotnetExecCommand());
     }
--- src/Toolchain/dotnet/dotnet.toml.cs
+++ src/Toolchain/dotnet/dotnet.toml.cs
@@ -2,5 +2,16 @@ namespace Zgo.Toolchain;
 
 public class DotnetToml
 {
-    
+
+}
+
+public enum DotnetRID
+{
+    None = 0,
+    Win_X64,
+    Linux_X64,
+    OSX_Arm64,
+    iOS_Arm64,
+    Android_Arm64
+
 }
